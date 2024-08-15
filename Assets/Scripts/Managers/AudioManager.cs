96e031f56fe4e5785b5aa28fff2b1d697b9c6bba
JulYakJul <137865717+JulYakJul@users.noreply.github.com>
2024-08-15 11:30:16 +0400

    Add Sound Setting

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        float savedVolume = PlayerPrefs.GetFloat("Volume", 1.0f);
+        SetVolume(savedVolume);
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        PlayerPrefs.SetFloat("Volume", volume);
+        PlayerPrefs.Save();
+    }
+}
